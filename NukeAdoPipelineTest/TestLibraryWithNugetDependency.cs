971a99d18861b93ae75d625e0c02ab7dc2e0d043
Benjamin Hart <bensmooth@users.noreply.github.com>
2025-04-30 23:47:28 -0500

    Initial add

---
+++ NukeAdoPipelineTest/TestLibraryWithNugetDependency.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace PublicNukeTest
+{
+    public class TestLibraryWithNugetDependency
+    {
+        public TestLibraryWithNugetDependency(string toSerialize)
+        {
+            Serialized = JsonConvert.SerializeObject(toSerialize);
+        }
+
+        public string Serialized { get; }
+    }
+}
